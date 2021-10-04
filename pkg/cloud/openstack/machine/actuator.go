472c551b8209e588e2d7d77935bb33c78f9f334f
Matthew Booth <mbooth@redhat.com>
2021-10-04 16:04:23 +0000

    Move some functions from actuator to utils

--- pkg/cloud/openstack/machine/actuator.go
+++ pkg/cloud/openstack/machine/actuator.go
@@ -25,21 +25,17 @@ import (
 
 	"sigs.k8s.io/cluster-api-provider-openstack/api/v1alpha4"
 	"sigs.k8s.io/cluster-api-provider-openstack/pkg/cloud/services/compute"
-	"sigs.k8s.io/cluster-api-provider-openstack/pkg/cloud/services/networking"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha4"
 
 	openstackconfigv1 "shiftstack/machine-api-provider-openstack/pkg/apis/openstackproviderconfig/v1alpha1"
 	"shiftstack/machine-api-provider-openstack/pkg/cloud/openstack"
 	"shiftstack/machine-api-provider-openstack/pkg/cloud/openstack/clients"
 
-	"github.com/gophercloud/gophercloud"
-	"github.com/gophercloud/utils/openstack/clientconfig"
 	machinev1 "github.com/openshift/machine-api-operator/pkg/apis/machine/v1beta1"
 	maoMachine "github.com/openshift/machine-api-operator/pkg/controller/machine"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/klog/v2"
-	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -72,70 +68,16 @@ func NewActuator(params openstack.ActuatorParams) (*OpenstackClient, error) {
 	}, nil
 }
 
-func (oc *OpenstackClient) getProviderClient(machine *machinev1.Machine) (*gophercloud.ProviderClient, *clientconfig.Cloud, error) {
+func (oc *OpenstackClient) getOpenStackContext(machine *machinev1.Machine) (*openStackContext, error) {
 	cloud, err := clients.GetCloud(oc.params.KubeClient, machine)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 	provider, err := clients.GetProviderClient(cloud, clients.GetCACertificate(oc.params.KubeClient))
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
-
-	return provider, &cloud, nil
-}
-
-func setMachineLabels(machine *machinev1.Machine, region, availability_zone, flavor string) {
-	// Don't update labels which have already been set
-	if machine.Labels[maoMachine.MachineRegionLabelName] != "" && machine.Labels[maoMachine.MachineAZLabelName] != "" && machine.Labels[maoMachine.MachineInstanceTypeLabelName] != "" {
-		return
-	}
-
-	if machine.Labels == nil {
-		machine.Labels = make(map[string]string)
-	}
-
-	// Set the region
-	machine.Labels[maoMachine.MachineRegionLabelName] = region
-
-	// Set the availability zone
-	machine.Labels[maoMachine.MachineAZLabelName] = availability_zone
-
-	// Set the flavor name
-	machine.Labels[maoMachine.MachineInstanceTypeLabelName] = flavor
-}
-
-type openStackContext struct {
-	provider *gophercloud.ProviderClient
-	cloud    *clientconfig.Cloud
-}
-
-func clientOptsForCloud(cloud *clientconfig.Cloud) *clientconfig.ClientOpts {
-	return &clientconfig.ClientOpts{
-		AuthInfo:   cloud.AuthInfo,
-		RegionName: cloud.RegionName,
-	}
-}
-
-func (osc *openStackContext) getComputeService() (*compute.Service, error) {
-	return compute.NewService(osc.provider, clientOptsForCloud(osc.cloud), ctrl.Log)
-}
-
-func (osc *openStackContext) getNetworkService() (*networking.Service, error) {
-	return networking.NewService(osc.provider, clientOptsForCloud(osc.cloud), ctrl.Log)
-}
-
-func (oc *OpenstackClient) getOpenStackContext(machine *machinev1.Machine) (*openStackContext, error) {
-	provider, cloud, err := oc.getProviderClient(machine)
-	if err != nil {
-		return nil, fmt.Errorf("error getting provider client for %s: %v", machine.Name, err)
-	}
-	return &openStackContext{provider, cloud}, nil
-}
-
-func getClusterNameWithNamespace(machine *machinev1.Machine) string {
-	clusterName := machine.Labels[machinev1.MachineClusterIDLabel]
-	return fmt.Sprintf("%s-%s", machine.Namespace, clusterName)
+	return &openStackContext{provider, &cloud}, nil
 }
 
 func getOSCluster() v1alpha4.OpenStackCluster {
@@ -368,6 +310,26 @@ func (oc *OpenstackClient) updateMachine(ctx context.Context, machine *machinev1
 	}
 
 	return oc.client.Status().Patch(ctx, machine, patch)
+}
+
+func setMachineLabels(machine *machinev1.Machine, region, availability_zone, flavor string) {
+	// Don't update labels which have already been set
+	if machine.Labels[maoMachine.MachineRegionLabelName] != "" && machine.Labels[maoMachine.MachineAZLabelName] != "" && machine.Labels[maoMachine.MachineInstanceTypeLabelName] != "" {
+		return
+	}
+
+	if machine.Labels == nil {
+		machine.Labels = make(map[string]string)
+	}
+
+	// Set the region
+	machine.Labels[maoMachine.MachineRegionLabelName] = region
+
+	// Set the availability zone
+	machine.Labels[maoMachine.MachineAZLabelName] = availability_zone
+
+	// Set the flavor name
+	machine.Labels[maoMachine.MachineInstanceTypeLabelName] = flavor
 }
 
 func updateStatus(machine *machinev1.Machine, instanceStatus *compute.InstanceStatus) error {
