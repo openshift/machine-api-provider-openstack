715c63680159f08ea33576e044b779c83dbae8e5
Pierre Prinetti <pierreprinetti@redhat.com>
2022-04-27 15:14:13 +0200

    Update cluster-api-provider-openstack to v0.6
    Notably, pull the ability to install with application credentials.
    
    In `go.mod`:
    * changed the CAPO `replace` to reference the latest commit
    * changed the cluster-api `replace` from `v1.0.2` to `v1.1.3` (which is
      what is referenced now in CAPO's go.mod)
    * replaced the imports of
      `sigs.k8s.io/cluster-api-provider-openstack/api/v1beta1` with
      `sigs.k8s.io/cluster-api-provider-openstack/api/v1alpha5` in the go
      files.

--- vendor/sigs.k8s.io/cluster-api-provider-openstack/pkg/cloud/services/networking/securitygroups.go
+++ vendor/sigs.k8s.io/cluster-api-provider-openstack/pkg/cloud/services/networking/securitygroups.go
@@ -23,7 +23,7 @@ import (
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/security/groups"
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/security/rules"
 
-	infrav1 "sigs.k8s.io/cluster-api-provider-openstack/api/v1beta1"
+	infrav1 "sigs.k8s.io/cluster-api-provider-openstack/api/v1alpha5"
 	"sigs.k8s.io/cluster-api-provider-openstack/pkg/record"
 )
 
@@ -404,7 +404,7 @@ func (s *Service) GetSecurityGroups(securityGroupParams []infrav1.SecurityGroupP
 
 		listOpts := groups.ListOpts(sg.Filter)
 		if listOpts.ProjectID == "" {
-			listOpts.ProjectID = s.projectID
+			listOpts.ProjectID = s.scope.ProjectID
 		}
 		listOpts.Name = sg.Name
 		listOpts.ID = sg.UUID
